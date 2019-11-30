8e3b11701897018276275c6f20d0a874d73e9b66
Vinod Vishwakarma <vinod@aigen.tech>
2019-11-30 10:45:02 +0530

    Project setup

---
+++ UIVVMaterialComponentTests/UIVVMaterialComponentTests.swift
@@ -0,0 +1,34 @@
+//
+//  UIVVMaterialComponentTests.swift
+//  UIVVMaterialComponentTests
+//
+//  Created by Vinod VIshwakarma on 29/11/2019.
+//  Copyright © 2019 Appinventor Inc http://appinventor.co.in. All rights reserved.
+//
+
+import XCTest
+@testable import UIVVMaterialComponent
+
+class UIVVMaterialComponentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
