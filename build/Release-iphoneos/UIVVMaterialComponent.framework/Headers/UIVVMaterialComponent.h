6ac1e069dadd05932096303d3b85c6cff6dde9e8
Vinod Vishwakarma <vinod@aigen.tech>
2019-12-30 19:48:09 +0530

    SDK document generated

---
+++ build/Release-iphoneos/UIVVMaterialComponent.framework/Headers/UIVVMaterialComponent.h
@@ -0,0 +1,18 @@
+//
+//  UIVVMaterialComponent.h
+//  UIVVMaterialComponent
+//
+//  Created by Vinod VIshwakarma on 29/11/2019.
+//  Copyright © 2019 Appinventor Inc http://appinventor.co.in. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIVVMaterialComponent.
+FOUNDATION_EXPORT double UIVVMaterialComponentVersionNumber;
+
+//! Project version string for UIVVMaterialComponent.
+FOUNDATION_EXPORT const unsigned char UIVVMaterialComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIVVMaterialComponent/PublicHeader.h>
+
---
+++ build/UIVVMaterialComponent.build/Release-iphoneos/UIVVMaterialComponent.build/Objects-normal/arm64/UIVVMaterialComponent_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/vinod/workspace/ui_lib/UIVVMaterialComponent/build/UIVVMaterialComponent.build/Release-iphoneos/UIVVMaterialComponent.build/DerivedSources/UIVVMaterialComponent_vers.c
