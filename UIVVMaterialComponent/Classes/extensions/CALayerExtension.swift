5fe5cdbfb8b4951c43de85030c5e62a9435c5806
Vinod Vishwakarma <vinod@aigen.tech>
2019-12-30 17:11:02 +0530

    code commit

--- UIVVMaterialComponent/Classes/extensions/CALayerExtension.swift
+++ UIVVMaterialComponent/Classes/extensions/CALayerExtension.swift
@@ -10,6 +10,7 @@ import UIKit
 
 @available(iOS 13.0, *)
 public extension CALayer {
+    
   func addShadow(color: UIColor, offset: CGSize, radius: CGFloat) {
     shadowColor = color.cgColor
     shadowOffset =  offset
@@ -17,6 +18,7 @@ public extension CALayer {
     shadowOpacity = 1
   }
   
+  @available(iOS 13.0, *)
   func addBottomBorder(color: UIColor = .separator, width: CGFloat = 1) {
     let borderLayer = CALayer()
     borderLayer.frame = CGRect(
