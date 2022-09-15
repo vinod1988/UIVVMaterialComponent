d443b5569696624b74b64639f4b2ed02cf7a729c
Vinod Vishwakarma <vinod@aigen.tech>
2022-09-15 21:40:00 +0530

    email validation issue fixed

--- Classes/extensions/UIViewControllerExtension.swift
+++ Classes/extensions/UIViewControllerExtension.swift
@@ -105,7 +105,7 @@ public extension UIViewController {
     
     
      func validateEmail(email: String) -> Bool {
-           let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+           let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,64}"
            let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
            return emailPredicate.evaluate(with: email)
        }
--- UIVVMaterialComponent.podspec
+++ UIVVMaterialComponent.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "UIVVMaterialComponent"
-  s.version      = "1.4.1"
+  s.version      = "1.4.2"
   s.summary      = "Beautiful Material Component for iOS Swift."
   s.description  = "The SDK is a completely customizable widget that can be used in any iOS app."
 
